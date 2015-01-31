9888196ece32fa27e472b1043721a6e1814626de
Nick Bargnesi <nbargnesi@selventa.com>
2015-01-31 00:15:59 -0500

    install the go shell only if needed

--- tests/all.sh
+++ tests/all.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+export TOP_DIR="$DIR"/../
 cd "$DIR" || exit 1
 
-# Install the latest go shell
-cd ..
-URL="https://raw.githubusercontent.com/formwork-io/gosh/latest/overlay.sh"
-wget --quiet --content-disposition "$URL" -O - | bash
+if [ ! -r "$TOP_DIR"/.gosh.sh ]; then
+    # Install the latest go shell
+    cd ..
+    URL="https://raw.githubusercontent.com/formwork-io/gosh/latest/overlay.sh"
+    wget --quiet --content-disposition "$URL" -O - | bash
+    cd "$DIR" || exit 1
+fi
 
-cd "$DIR" || exit 1
-export CONTRIB_DIR="$DIR"/../
+export TOP_DIR="$DIR"/../
 for test_script in *.sh; do
     if [ "$test_script" == "all.sh" ]; then
         continue
