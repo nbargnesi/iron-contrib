a3e832f21abdbfacc64fd1b3a01c4472390bb235
Nick Bargnesi <nbargnesi@selventa.com>
2015-01-31 00:10:56 -0500

    install the latest go shell before unit tests

--- tests/all.sh
+++ tests/all.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$DIR" || exit 1
+
+# Install the latest go shell
+cd ..
+URL="https://raw.githubusercontent.com/formwork-io/gosh/latest/overlay.sh"
+wget --quiet --content-disposition "$URL" -O - | bash
+
+cd "$DIR" || exit 1
 export CONTRIB_DIR="$DIR"/../
 for test_script in *.sh; do
     if [ "$test_script" == "all.sh" ]; then
