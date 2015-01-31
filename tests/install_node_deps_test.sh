0444d1875ceca2eaea2d1df8f4a247d792064f66
Nick Bargnesi <nbargnesi@selventa.com>
2015-01-31 00:16:14 -0500

    flesh out first test

--- tests/install_node_deps_test.sh
+++ tests/install_node_deps_test.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
-source "$CONTRIB_DIR"/node.sh || exit 1
+source "$TOP_DIR"/env.sh || exit 1
+source "$TOP_DIR"/node.sh || exit 1
 
-install_node_deps
+testInstallNodeDepsFailure {
+    install_node_deps 1>/dev/null 2>&1
+    # assert 
+    assertEquals $? 1
+}
 
 . "$(dirname "$BASH_SOURCE[0]")"/shunit2-2.1.6/src/shunit2
