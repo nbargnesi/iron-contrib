d6e2e9625c863ff14afd73c9507c26595821a9d0
Nick Bargnesi <nbargnesi@selventa.com>
2015-02-10 18:21:52 -0500

    allow updating formwork-io/gosh for test purposes

--- tests/all.sh
+++ tests/all.sh
@@ -1,17 +1,38 @@
 #!/usr/bin/env bash
-DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-export TOP_DIR="$DIR"/../
-cd "$DIR" || exit 1
+dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+export TOP_DIR="$dir"/../
+cd "$dir" || exit 1
 
-if [ ! -r "$TOP_DIR"/.gosh.sh ]; then
+_getopt=$(getopt -o U --long update-gosh -- "$@") || exit 1
+update_gosh="no"
+while [ $# -gt 0 ]; do
+    case "$1" in
+        "-U")
+            ;&
+        "--update-gosh")
+            update_gosh="yes"
+            ;;&
+    esac
+    shift
+done
+
+if [ ! -r "$TOP_DIR"/.gosh.sh -o "$update_gosh" == "yes" ]; then
+    echo -n "Updating formwork-io/gosh... "
     # Install the latest go shell
     cd ..
-    URL="https://raw.githubusercontent.com/formwork-io/gosh/latest/overlay.sh"
-    wget --quiet --content-disposition "$URL" -O - | bash
-    cd "$DIR" || exit 1
+    url="https://raw.githubusercontent.com/formwork-io/gosh/latest/overlay.sh"
+    output=$(wget --quiet --content-disposition "$url" -O - | bash 2>&1)
+    if [ $? -ne 0 ]; then
+        echo "FAIL"
+        echo "$output"
+        exit 1
+    fi
+    echo "done"
+    echo "--"
+    cd "$dir" || exit 1
 fi
 
-export TOP_DIR="$DIR"/../
+export TOP_DIR="$dir"/../
 for test_script in *.sh; do
     if [ "$test_script" == "all.sh" ]; then
         continue
